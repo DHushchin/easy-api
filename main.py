4904cb9bd3374bd425e998fcafaac527ae953346
Dmytro Hushchin <dhuchchin@gmail.com>
2024-02-12 23:54:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 load_dotenv('.env')
 
 @app.route('/')
 def get_env_variable():
     env_variable = os.getenv('MY_ENV_VARIABLE')
-    return env_variable
+    return jsonify({'baseUrl': env_variable})
 
 if __name__ == '__main__':
     app.run()
